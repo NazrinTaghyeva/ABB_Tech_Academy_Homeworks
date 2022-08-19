5242b1fa57fc2fcf1fc54f4674bb27c4d006c15b
Nazryn <80715402+NazrinTaghyeva@users.noreply.github.com>
2022-08-19 21:33:17 +0400

    Add files via upload

---
+++ Happy_Family_Homework_5/src/main/Main.java
@@ -0,0 +1,17 @@
+package main;
+
+public class Main {
+    public static void main(String[] args) {
+        Pet pet = new Pet("Dog","Cookie",2,84, new String[]{"eating","playing"});
+        System.out.println(pet.toString());
+        Human mother = new Human("Kate","Gomez" , 1975);
+        Human father = new Human("John","Gomez" , 1970);
+        Human child = new Human("Selena","Gomez",2002,85,pet, mother,father,new String[][]{{"monday","meet the friends"},{"sunday","watch the film"}});
+        Family family = new Family(mother,father);
+        System.out.println(child.toString());
+        System.out.println(family.toString());
+        family.addChild(child);
+
+
+    }
+}
